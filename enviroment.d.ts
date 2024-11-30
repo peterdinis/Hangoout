5a7ef14b66dc1ac27679898f84500b8f04b645b4
Peter Dinis <peterdinis0@pobox.sk>
2024-11-30 17:02:42 +0100

    infra: Small update in enviroment.d.ts

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -2,7 +2,6 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             CONVEX_DEPLOYMENT: string;
-            NODE_ENV: 'development' | 'production';
             NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
             CLERK_SECRET_KEY: string;
             NEXT_PUBLIC_CONVEX_URL: string;

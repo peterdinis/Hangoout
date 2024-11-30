f5c2aa419e4a33d48693fffdf9a9cfb8dca47660
Peter Dinis <peterdinis0@pobox.sk>
2024-11-30 09:52:45 +0100

    intra: Fixing hydration error

--- app/_components/providers/ThemeProvider.tsx
+++ app/_components/providers/ThemeProvider.tsx
@@ -4,9 +4,22 @@ import {
 	ThemeProvider as NextThemesProvider,
 	type ThemeProviderProps,
 } from "next-themes";
+import { useEffect, useState } from "react";
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	if (!isClient) return null;
+
+	return (
+		<NextThemesProvider attribute="class" {...props}>
+			{children}
+		</NextThemesProvider>
+	);
 };
 
 export default ThemeProvider;

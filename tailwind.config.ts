e1c1c422c59b24e5c5f80efb47f5f3dfe28b5377
Peter Dinis <peterdinis0@pobox.sk>
2024-11-30 11:00:24 +0100

    feat: Working on navigation WIP

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,5 +58,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
+	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;

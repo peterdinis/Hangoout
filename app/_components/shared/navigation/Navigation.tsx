8a07d64c7c6898e92add4fa0b421559b4c46be7c
Peter Dinis <peterdinis0@pobox.sk>
2024-11-30 14:25:35 +0100

    feat: Updating Navigation

--- app/_components/shared/navigation/Navigation.tsx
+++ app/_components/shared/navigation/Navigation.tsx
@@ -1,36 +1,40 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import type { FC } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import NavigationLogo from "./NavigationLogo";
 import NavigationSearch from "./NavigationSearch";
 
 const Navigation: FC = () => {
-  return (
-    <section className="border-b">
-      <div className="flex flex-col lg:flex-row items-center gap-4 p-4">
-        <div className="flex items-center justify-between w-full lg:w-auto">
-          <Link href={"/"} className="flex items-center gap-2 font-bold shrink-0">
-            <NavigationLogo />
-            <span className="text-2xl prose-h2: font-extrabold text-gradient bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
-              Hangooout
-            </span>
-          </Link>
+	return (
+		<section className="border-b">
+			<div className="flex flex-col lg:flex-row items-center gap-4 p-4">
+				<div className="flex items-center justify-between w-full lg:w-auto">
+					<Link
+						href={"/"}
+						className="flex items-center gap-2 font-bold shrink-0"
+					>
+						<NavigationLogo />
+						<span className="text-2xl prose-h2: font-extrabold text-gradient bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+							Hangooout
+						</span>
+					</Link>
 
-          <div className="lg:hidden">
-            {/* TODO: Clerk button */}
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-        </div>
+					<div className="lg:hidden">
+						<SignedOut>
+							<SignInButton />
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</div>
+				</div>
 
-        <div className="w-full flex justify-center items-center lg:max-w-2xl">
-            <NavigationSearch />
-        </div>
-      </div>
-    </section>
-  );
+				<div className="w-full flex justify-center items-center lg:max-w-2xl">
+					<NavigationSearch />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Navigation;
--- app/_components/shared/navigation/NavigationSearch.tsx
+++ app/_components/shared/navigation/NavigationSearch.tsx
@@ -1,25 +1,25 @@
-import { FC } from "react";
-import Form from "next/form";
-import { Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import Form from "next/form";
+import type { FC } from "react";
 
 const NavigationSearch: FC = () => {
-  return (
-    <Form action={"/search"} className="relative">
-      <input
-        type="text"
-        name="q"
-        className="w-full py-3 px-4 pl-12 bg-white rounded-xl border border-gray-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:broder-transparent transition-all duration-200"
-      />
-      <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
-      <Button
-        type="submit"
-        className="absolute right-3 top-1/2 -translate-y-1/2 bg-blue-600 text-slate-50 px-4 py-1.5 rounded-lg font-medium hover:bg-blue-700 transition-colors duration-200"
-      >
-        Search
-      </Button>
-    </Form>
-  );
+	return (
+		<Form action={"/search"} className="relative">
+			<input
+				type="text"
+				name="q"
+				className="w-full py-3 px-4 pl-12 bg-white rounded-xl border border-gray-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:broder-transparent transition-all duration-200"
+			/>
+			<Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+			<Button
+				type="submit"
+				className="absolute right-3 top-1/2 -translate-y-1/2 bg-blue-600 text-slate-50 px-4 py-1.5 rounded-lg font-medium hover:bg-blue-700 transition-colors duration-200"
+			>
+				Search
+			</Button>
+		</Form>
+	);
 };
 
 export default NavigationSearch;
